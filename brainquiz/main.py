09f9566f45b1263a5a445aff2dd29905f8491f7e
Okkazeri <vaga2145@gmail.com>
2023-06-24 17:31:27 +0300

    one more quiz

---
+++ brainquiz/main.py
@@ -0,0 +1,13 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+if __name__ == "__main__":
+    question_bank = [Question(item['question'], item['correct_answer']) for item in question_data]
+
+    quiz = QuizBrain(question_bank)
+    while quiz.still_has_questions():
+        quiz.next_question()
+
+    print(f"You`ve completed the quiz\n"
+          f"You final score was: {quiz.score}/{quiz.question_number}")
